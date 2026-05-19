0af624c81bf0fdd77bdf1f6334c800da9aa0b3d3
Mark LaPointe <mark@cloudbsd.org>
2026-05-19 17:40:49 -0500

    legal: update company ownership to REVYTECH, Inc.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   projectName: "CloudBSD",
-  companyName: "CloudBSD",
+  companyName: "REVYTECH, Inc.",
   ownerName: "Mark LaPointe",
   ownerEmail: "mark@cloudbsd.org",
   discordLink: "https://discord.gg/8KYawCuG",
