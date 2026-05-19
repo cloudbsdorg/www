853a18849bb095e9280ddb293692b9bf0191ff61
Mark LaPointe <mark@cloudbsd.org>
2026-05-19 17:42:10 -0500

    fix: remove double period in copyright footer

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   projectName: "CloudBSD",
-  companyName: "REVYTECH, Inc.",
+  companyName: "REVYTECH, Inc",
   ownerName: "Mark LaPointe",
   ownerEmail: "mark@cloudbsd.org",
   discordLink: "https://discord.gg/8KYawCuG",
